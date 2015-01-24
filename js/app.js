30d83aebf886403eb3041c46be7df80cd4f0147f
Kevin Solorio <kevin@detone8.com>
2015-01-24 14:06:34 -0600

    setup directory structure to match todomvc-flux repo

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ var requireStylesheet = require('stylesheets').requireStylesheet;
 
 requireStylesheet(process.env.STATIC_ROOT + 'MyComponent.css');
 
-var MyComponent = React.createClass({
+var MyComponent = React.createClass({displayName: 'MyComponent',
   render: function() {
-    return <span className="MyComponent">Hello, MyComponent!</span>;
+    return React.DOM.span( {className:"MyComponent"}, "Hello, MyComponent!");
   }
 });
 
